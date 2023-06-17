427f733203494cd1723da1a2d46a88d2920f3c6c
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-17 14:04:36 +0530

    updating roboshop-shared-library directory

--- vars/common.groovy
+++ vars/common.groovy
@@ -9,11 +9,13 @@ def compile() {
 }
 
 def testcases() {
-    if(app_lang == "nodejs") {
-        sh 'echo test'
-    }
+   //npm test for nodejs
+   //mvn test for maven
+   //python -m unittests
+   //go test for golang
+   sh 'echo ok'
+}
 
-    if(app_lang == "maven"){
-        sh 'echo test'
-    }
+def codequality() {
+    sh "sonar-scanner -Dsonar.host.url=http://172.31.94.171:9000 -Dsonar.login=admin -Dsonar.password=admin123 -Dsonar.projectkey=${component}"
 }
