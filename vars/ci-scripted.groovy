14180121b3dfbf62e654aa6841bb206561537c4e
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-06-21 17:27:09 +0530

    updating roboshop-shared-library directory

---
+++ vars/ci-scripted.groovy
@@ -0,0 +1,26 @@
+def call() {
+    if (!env.sonar_extra_opts) {
+        env.sonar_extra_opts = ""
+    }
+
+    node('workstation') {
+
+        try {
+
+            stage('Compile/Build') {
+                common.compile()
+            }
+
+            stage('Test Cases') {
+                common.testcases()
+            }
+
+            stage('Code Quality') {
+                common.codequality()
+            }
+
+        } catch (e) {
+            mail bcc: '', body: "<h1>${component} - Pipe Line Failed \n ${BUILD_URL}</h1>", cc: '', from: 'praveen.gaju94@gmail.com', replyTo: '', subject: "${component} - Pipe Line Failed", to: 'praveen.gaju94@gmail.com', mimeType: 'text/html'
+        }
+    }
+}
