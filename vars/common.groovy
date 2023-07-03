56b72b0e7a375097ce64fd4f8424ccee8365a98e
Praveen Gajulapalle <Praveen.gaju8994@gmail.com>
2023-07-03 20:10:29 +0530

    updating roboshop-shared-library directory

--- vars/common.groovy
+++ vars/common.groovy
@@ -24,14 +24,14 @@ def codequality() {
 
 def prepareArtifacts() {
     sh 'echo ${TAG_NAME} >VESION'
-    if (app_lang == "nodejs" || app_lang == "angular") {
+    //if (app_lang == "nodejs" || app_lang == "angular") {
         sh 'zip -r ${component}-${TAG_NAME}.zip * -x Jenkinsfile'
-    }
+    //}
 }
 
 def artifactUpload() {
     sh 'echo ${TAG_NAME} >VESION'
-    if (app_lang == "nodejs" || app_lang == "angular") {
+    //if (app_lang == "nodejs" || app_lang == "angular") {
         sh 'curl -v -u admin:admin123 --upload-file ${component}-${TAG_NAME}.zip http://172.31.86.34:8081/repository/${component}/${component}-${TAG_NAME}.zip'
-    }
+    //}
 }
